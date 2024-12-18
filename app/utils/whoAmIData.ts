7695467ca4121ceea17944ff7cbe0682e6d38b05
Aasimtricky <82271411+Aasimtricky@users.noreply.github.com>
2024-12-18 13:02:03 +0530

    2Update whoAmIData.ts

--- app/utils/whoAmIData.ts
+++ app/utils/whoAmIData.ts
@@ -4,5 +4,6 @@ export const whoAmIData: whoAmIDataType = {
   fullName: "Leo Miranda",
   profession: "Frontend developer",
   whoAmI:
-    "I'm currently based in Atlanta, exploring career opportunities across the USA. With extensive experience in Product Management and UI/UX design, I have successfully led a variety of projects in sectors ranging from websites and games to healthcare and e-commerce. I am also advancing my skills through a Master’s in IT Project Management at Cambellsville University. ",
+    "I'm currently based in Atlanta, exploring career opportunities across the USA. With extensive experience in Product Management and UI/UX design, I have successfully led a variety of projects in sectors ranging from websites and games to healthcare and e-commerce. I am also advancing my skills through a Master’s in IT Project Management at Cambellsville University.",
+  quote: "Design is intelligence made visible." // Added the required `quote` property
 };
