60ff83e805e366eb897b5fe7b798e2fcba09c3c5
Aasimtricky <82271411+Aasimtricky@users.noreply.github.com>
2024-12-18 13:23:11 +0530

    title Update whoAmIData.ts

--- app/utils/whoAmIData.ts
+++ app/utils/whoAmIData.ts
@@ -1,8 +1,8 @@
 import { whoAmIDataType } from "../types/whoAmIData";
 
 export const whoAmIData: whoAmIDataType = {
-  fullName: "Leo Miranda",
-  profession: "Frontend developer",
+  fullName: "Mohammed Aasim",
+  profession: "Product Manager",
   whoAmI:
     "I'm currently based in Atlanta, exploring career opportunities across the USA. With extensive experience in Product Management and UI/UX design, I have successfully led a variety of projects in sectors ranging from websites and games to healthcare and e-commerce. I am also advancing my skills through a Master’s in IT Project Management at Cambellsville University.",
   quote: "Design is intelligence made visible." // Added the required `quote` property
