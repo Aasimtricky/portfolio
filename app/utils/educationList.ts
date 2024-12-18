901a4095c83386815eae3bf67e27c9256366d458
Aasimtricky <82271411+Aasimtricky@users.noreply.github.com>
2024-12-18 14:09:47 +0530

    2Update educationList.ts

--- app/utils/educationList.ts
+++ app/utils/educationList.ts
@@ -2,18 +2,18 @@ import { educationType } from "../types";
 
 export const educationList: educationType[] = [
   {
-    career: "Product & Sales",
-    years: "CSCS, 2023",
+    career: "CSCS",
+    years: "Product & Sales, 2023",
     description: "",
   },
   {
-    career: "Founder",
-    years: "Hardware Shack, Oct 2021",
+    career: "Hardware Shack",
+    years: "Founder, Oct 2021 -",
     description: "",
   },
   {
-    career: "Associate Project Manager",
-    years: "Simplex Pharmacy, May 2023 - Dec 2023",
+    career: "Simplex Pharmacy",
+    years: "Associate Project Manager, May 2023 - Dec 2023",
     description: "cd",
   },
 ];
