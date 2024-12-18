0bd146b0003d92ddc00f6e8cfcc950e6bac5e9a1
Aasimtricky <82271411+Aasimtricky@users.noreply.github.com>
2024-12-18 12:57:42 +0530

    Update whoAmIData.ts

--- app/utils/whoAmIData.ts
+++ app/utils/whoAmIData.ts
@@ -4,6 +4,5 @@ export const whoAmIData: whoAmIDataType = {
   fullName: "Leo Miranda",
   profession: "Frontend developer",
   whoAmI:
-    "Hello! I am a developer who really enjoys transforming ideas into functional software that exceeds expectations. I like to be constantly learning from other professionals, teams and communities to stay on top of the latest industry trends. Being a developer is not just a job for me; it is an activity that I absolutely appreciate all the time.",
-  quote: "I really like creating software solutions",
+    "I'm currently based in Atlanta, exploring career opportunities across the USA. With extensive experience in Product Management and UI/UX design, I have successfully led a variety of projects in sectors ranging from websites and games to healthcare and e-commerce. I am also advancing my skills through a Master’s in IT Project Management at Cambellsville University. ",
 };
